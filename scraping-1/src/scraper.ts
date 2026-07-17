294e658f7b89775d68ed23ab59a81feeb95e5a4b
sharif <99sharif00@gmail.com>
2026-07-17 11:04:13 +0530

    found logic for images

--- scraping-1/src/scraper.ts
+++ scraping-1/src/scraper.ts
@@ -2,6 +2,12 @@ import * as cheerio from 'cheerio';
 import {Painting}  from './types';
 import * as fs from 'fs'
 
+
+
+// function buildDeferredImageMap(html: string): Record<string, string> {
+  
+// }
+
 function extractPaintings(path:string): Painting[] {
   const html = fs.readFileSync(path, 'utf-8');
   const $ = cheerio.load(html);
@@ -9,13 +15,39 @@ function extractPaintings(path:string): Painting[] {
   const domain = "https://www.google.com"
 
 
-  const items = $('a').filter((_ , el)=>{ // filtering anchor tags which has img tag and at least 2 divs as direct descendants
+  // Find script tags which has base64 images 
+  const scriptTags = $('script').filter((_,el)=>{
+    const scriptContent = $(el).html() ?? "";
+    return scriptContent.includes('data:image/jpeg;base64') && scriptContent.includes('var ii') && scriptContent.includes('_setImagesSrc');
+  })
+
+  console.log("total: scriptTags:",scriptTags.length, $(scriptTags[0]).html() ?? "No script content found")
+
+  console.log("regex for url:", $(scriptTags[0]).html()?.match(/data:image\/[^']+/)?.[0] ?? "No URL found")
+  
+  console.log("regex for url:", $(scriptTags[0]).html()?.match(/var\s+ii\s*=\s*\[([^\]]+)\]/)?.[0] ?? "No URL found")
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // filtering anchor tags which has img tag and at least 2 divs as direct descendants
+  const items = $('a').filter((_ , el)=>{ 
       const $eachAnchor = $(el);
       return $eachAnchor.find('img').length > 0 && $eachAnchor.find('div > div').length >= 2 && ($eachAnchor.attr("href")?.startsWith("/search") ?? false) && ($eachAnchor.attr("href")?.includes("stick")!==undefined ? true :false);  
   }) 
 
   console.log("total: Actual Anchor items:",items.length)
   
+  // -------------------- Travel on the items -----------
   items.each((_,el)=>{
     const $eachAnchor = $(el);
     const container_holds_divs = $eachAnchor.find('div > div'); // finding div which has divs as direct descendants
